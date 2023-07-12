9f512fe357ad77b52326c52498a5e6d171eaf3b6
mztikk <mztikk@outlook.de>
2023-07-12 18:11:24 +0200

    DirEntryFilter trait renamed to IgnoreDirEntry, DirEntryFilter now enum with all possible variants

--- src/file_only.rs
+++ src/file_only.rs
@@ -1,17 +1,18 @@
-use crate::DirEntryFilter;
+use crate::IgnoreDirEntry;
 
 /// A filter that matches only files.
-#[derive(Default)]
+#[derive(Copy, Clone, Default, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct FileOnlyFilter;
 
 impl FileOnlyFilter {
     /// Creates a new file only filter
     pub fn new() -> Self {
-        FileOnlyFilter::default()
+        Self::default()
     }
 }
 
-impl DirEntryFilter for FileOnlyFilter {
+impl IgnoreDirEntry for FileOnlyFilter {
     fn ignore(&self, entry: &std::fs::DirEntry) -> bool {
         match entry.file_type() {
             Ok(file_type) => file_type.is_dir() || file_type.is_symlink(),
--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,84 @@
 //! This library provides a simple interface for creating and using filters.
 //! Filters are used to ignore DirEntry.
 
+/// Filter for matching only directories
+mod directory_only;
+/// Filter for matching only files
+mod file_only;
+
+pub use directory_only::DirectoryOnlyFilter;
+pub use file_only::FileOnlyFilter;
+
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use std::fs::DirEntry;
 
-/// Filter for matching only directories
-pub mod directory_only;
-/// Filter for matching only files
-pub mod file_only;
-
 /// Provides an interface filtering and ignoring DirEntry.
-pub trait DirEntryFilter {
-    /// Returns `true` if the path should be ignored.
+pub trait IgnoreDirEntry {
+    /// Returns `true` if the DirEntry should be ignored.
     fn ignore(&self, entry: &DirEntry) -> bool;
+}
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+/// An enum that represents different types of filters for ignoring dir entries.
+pub enum DirEntryFilter {
+    /// Filter that matches files.
+    FileOnly(FileOnlyFilter),
+    /// Filter that matches directories.
+    DirectoryOnly(DirectoryOnlyFilter),
+}
+
+impl From<FileOnlyFilter> for DirEntryFilter {
+    fn from(value: FileOnlyFilter) -> Self {
+        DirEntryFilter::FileOnly(value)
+    }
+}
+
+impl From<DirectoryOnlyFilter> for DirEntryFilter {
+    fn from(value: DirectoryOnlyFilter) -> Self {
+        DirEntryFilter::DirectoryOnly(value)
+    }
+}
+
+impl DirEntryFilter {
+    /// Creates a new `DirEntryFilter` for `FileOnlyFilter`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use direntryfilter::DirEntryFilter;
+    ///
+    /// let filter = DirEntryFilter::new_file();
+    /// ```
+    pub fn new_file() -> Self {
+        FileOnlyFilter::default().into()
+    }
+
+    /// Creates a new `DirEntryFilter` for `DirectoryOnlyFilter`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use direntryfilter::DirEntryFilter;
+    ///
+    /// let filter = DirEntryFilter::new_directory();
+    /// ```
+    pub fn new_directory() -> Self {
+        DirectoryOnlyFilter::default().into()
+    }
+}
+
+impl IgnoreDirEntry for DirEntryFilter {
+    fn ignore(&self, entry: &DirEntry) -> bool {
+        match self {
+            DirEntryFilter::FileOnly(x) => x.ignore(entry),
+            DirEntryFilter::DirectoryOnly(x) => x.ignore(entry),
+        }
+    }
+}
+
+impl<T: AsRef<[DirEntryFilter]>> IgnoreDirEntry for T {
+    fn ignore(&self, entry: &DirEntry) -> bool {
+        self.as_ref().par_iter().any(|filter| filter.ignore(entry))
+    }
 }
